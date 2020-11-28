932b1708d963a6e4a1198cfd20ba01787ccdef0d
= <jmcmasters411@gmail.com>
2020-11-28 15:39:56 -0500

    Complete up to equals() method

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -4,19 +4,35 @@ class ArrayUtils {
   }
 
   append(original, value) {
+    original.push(value);
     return original;
   }
 
   clone(original) {
-    return original;
+    let copyArray = [];
+    for (let e of original) {
+      copyArray.push(e);
+    }
+    return copyArray;
   }
 
   subArray(original, from, to) {
-    return original;
+    const newArray = [];
+    for (let i = from; i < to; i++) {
+      if (i < original.length) {
+        newArray.push(original[i]);
+      } else {
+        break;
+      }
+    }
+    return newArray;
   }
 
   equals(arr1, arr2) {
-    return false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
   }
 
   fill(original, value) {}
@@ -32,8 +48,13 @@ class ArrayUtils {
   reverse(original) {}
 }
 
-let arrayUtils = new ArrayUtils();
+const arrayUtils = new ArrayUtils();
+let array = [1, 2, 3, 4, 5];
+let array2 = [1, 2, 3, 4, 5];
+let array3 = ["cat", "dog", "penguine"];
 
-let arr = [1, 2, 3, 4, 5];
-let arr2 = [];
-console.log(arrayUtils.isEmpty(arr));
+console.log(arrayUtils.isEmpty(array));
+console.log(arrayUtils.append(array, 6));
+console.log(arrayUtils.clone(array));
+console.log(arrayUtils.subArray(array, 2, 4));
+console.log(arrayUtils.equals(array, array2)); // should return false because append() adds another element to the first array.
