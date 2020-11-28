e6d79230322259e40d08188f795c81e598f3bdb4
Michael <Michael@Stephanies-MBP.fios-router.home>
2020-11-28 15:19:39 -0500

    Create index.html. Complete and call first function

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,21 +1,39 @@
-class ArrayUtils{
+class ArrayUtils {
+  isEmpty(array) {
+    return array.length == 0;
+  }
 
-    isEmpty(array){return false;}
+  append(original, value) {
+    return original;
+  }
 
-    append(original, value){return original;}
+  clone(original) {
+    return original;
+  }
 
-    clone(original){return original;}
+  subArray(original, from, to) {
+    return original;
+  }
 
-    subArray(original, from, to){return original;}
+  equals(arr1, arr2) {
+    return false;
+  }
 
-    equals(arr1, arr2){return false;}
+  fill(original, value) {}
 
-    fill(original, value){}
+  indexOf(original, value) {
+    return -1;
+  }
 
-    indexOf(original, value){return -1;}
+  remove(original, value) {
+    return original;
+  }
 
-    remove(original, value){return original;}
-
-    reverse(original){}
-    
+  reverse(original) {}
 }
+
+let arrayUtils = new ArrayUtils();
+
+let arr = [1, 2, 3, 4, 5];
+let arr2 = [];
+console.log(arrayUtils.isEmpty(arr));
