a2b6251dff84d37880049fd929f802bf187b3a53
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-30 18:17:52 -0500

    All methods completed

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,21 +1,93 @@
 class ArrayUtils{
+   
+    // Step 1
+    empty(array){
+        if (array.length === 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    
+// Step 2
+    append(original, value){
+        original.push[value];
+        return original;
+    }
 
-    isEmpty(array){return false;}
+    // Step 3
+    clone(array){
+        let newArray = [];
+        newArray = array;
+        return newArray;
+    }
 
-    append(original, value){return original;}
+    // Step 4
+    subArray(original, from, to){
+        return original;
+        const newArray = [];
+        for (let i = from; i < to; i++) {
+          if (i < original.length) {
+            newArray.push(original[i]);
+          } else {
+            break;
+          }
+        }
+        return newArray;
+      }
 
-    clone(original){return original;}
+ 
 
-    subArray(original, from, to){return original;}
 
-    equals(arr1, arr2){return false;}
+    // Step 5
+    equals(arr1, arr2){   
+        if(array === array2){
+        return true;
+    } else{
+        return false;
+    }
+}
+    
+    // Step 6
+    fill(original, value){
+        for(i = 0; i < original.length; i++){
+            original[i] = value;
+        }   return original;
+    }
 
-    fill(original, value){}
+    // Step 7
+    indexOf(original, value){
+        for(i = 0; i < original.length; i++){
+            let i= original[1];
+           if(original[i] == value){
+            return i;
+        }
+        } return -1;
+    }
 
-    indexOf(original, value){return -1;}
+    // Step 8
+    remove(original, value){ 
+        return original;
+        let newArray = [];
+        for (let e of original) {
+          if (e !== value) newArray.push(e);
+        }
+    
+        return newArray;
+}
 
-    remove(original, value){return original;}
 
-    reverse(original){}
+    // Step 9
+    reverseArray(arr) {
+        let newArray = [];
+        for (i = arr.length - 1; i >= 0; i--) {
+          newArray.push(arr[i]);
+        }
+        return newArray;
+      }
     
 }
+
+
+// Create reference
+const arrayUtils = new ArrayUtils();
